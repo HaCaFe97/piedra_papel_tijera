46e0df5ece946565ea77dc0cdbc5ecabbf91fd7b
Harry Caicedo <hacafe97@gmail.com>
2024-01-30 22:11:57 -0500

    Se añadieron imagenes de piedra, papel y tijera, se adaptó el código de main.py para mostrar imágenes en los botones

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 import tkinter as tk
 from formatos_textos import estilos_T1, estilos_T2, estilos_T3, estilos_T4, estilos_T5
+from PIL import Image, ImageTk
 
 #Caracteristicas de Ventana
 ventana1 = tk.Tk()
-ventana1.state("zoomed")
 ventana1.title("Piedra, Papel o Tijera")
 
-#Titutlo del programa en label
+#Imágenes de los botones
+piedra = Image.open("Piedra.png")
+papel = Image.open("Papel.png")
+tijera = Image.open("Tijera.png")
+
+piedra = piedra.resize((150, 221))
+papel = papel.resize((150, 221))
+tijera = tijera.resize((150, 221))
+
+piedra_tk = ImageTk.PhotoImage(piedra)
+papel_tk = ImageTk.PhotoImage(papel)
+tijera_tk = ImageTk.PhotoImage(tijera)
+
+#Textos en la ventana
 titulo = tk.Label(ventana1, estilos_T1)
 titulo.pack(pady=0)
 
@@ -22,10 +35,19 @@ etiqueta3.pack(pady=0)
 etiqueta4= tk.Label(ventana1, estilos_T5)
 etiqueta4.pack(side="bottom", pady=0)
 
-#Botones
-boton1 = tk.Button(ventana1, text="Haz clic aquí")
-boton1["command"] = lambda: etiqueta1.config(text='Este botón funciona')
-boton1.pack(pady=0)
+#Frame para botones
+frame_botones = tk.Frame(ventana1)
+frame_botones.pack(expand=True)
+
+#Botón
+boton1 = tk.Button(frame_botones, image=piedra_tk,text="Piedra")
+boton1.pack(side="left", padx=(0, 5), pady=0)
+
+boton2 = tk.Button(frame_botones,image=papel_tk, text="Papel")
+boton2.pack(side="left", padx=(0, 5), pady=0)
+
+boton3 = tk.Button(frame_botones,image=tijera_tk, text="Tijera")
+boton3.pack(side="left", padx=(0, 5), pady=0)
 
 #Loop de ejecución de ventana
 ventana1.mainloop()
