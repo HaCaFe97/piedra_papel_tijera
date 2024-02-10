3fb261da74132b9c7d1e74ccb437d8f6002e4c2e
Harry Caicedo <hacafe97@gmail.com>
2024-02-09 22:02:16 -0500

    Se han añadido todos los estilos de texto. El programa en este punto puede considerarse en fase PMV. Aún se pueden mejorar muchísimas cosas... Continuara

--- formatos_textos.py
+++ formatos_textos.py
@@ -1,71 +1,77 @@
 #Estilos de textos
-estilos_T1 = {
+titulo_nivel1 = {
   "text" : "Piedra, Papel o Tijera",
   "font" : ('Times New Roman', 18, 'bold'),
   "foreground" : "purple"
 }
 
-estilos_T2 = {
+titulo_nivel2 = {
   "text" : "Juega contra la computadora en un juego de piedra, papel o tijera",
   "font" : ('Times New Roman', 16, 'bold'),
   "foreground" : "blue"
 }
 
-estilos_T3 = {
+titulo_nivel3 = {
   "text" : "Quien gane dos de tres juegos, gana el match",
   "font" : ('Times New Roman', 14, 'bold'),
   "foreground" : "red"
 }
 
-estilos_T4 = {
+texto_normal1 = {
   "text" : "Elige una opción:",
   "font" : ('Times New Roman', 12, 'bold'),
   "foreground" : "black"
 }
 
-estilos_T5 = {
-  "text" : "RONDA",
+texto_normal2 = {
+  "text" : "Victorias jugador",
   "font" : ('Times New Roman', 12, 'bold'),
   "foreground" : "black"
 }
 
-estilos_T6 = {
+texto_normal3 = {
+  "text" : "ESTÁS EN LA RONDA",
+  "font" : ('Times New Roman', 12, 'bold'),
+  "foreground" : "black"
+}
+
+texto_normal4 = {
+  "text" : "El jugador escogió",
+  "font" : ('Times New Roman', 12, 'bold'),
+  "foreground" : "black"
+}
+
+texto_normal5 = {
+  "text" : "Ganador de la ronda",
+  "font" : ('Times New Roman', 12, 'bold'),
+  "foreground" : "black"
+}
+
+texto_normal6 = {
+  "text" : "La computadora escogió",
+  "font" : ('Times New Roman', 12, 'bold'),
+  "foreground" : "black"
+}
+
+texto_normal7 = {
   "text" : "La computadora elegirá",
   "font" : ('Times New Roman', 12, 'bold'),
   "foreground" : "black"
 }
 
-estilos_T7 = {
-  "text" : "El jugador escogió",
-  "font" : ('Times New Roman', 12),
-  "foreground" : "black"
-}
-
-estilos_T8 = {
-  "text" : "Ganador de la ronda",
-  "font" : ('Times New Roman', 12),
-  "foreground" : "black"
-}
-
-estilos_T9 = {
-  "text" : "La computadora escogió",
-  "font" : ('Times New Roman', 12),
-  "foreground" : "black"
-}
-
-estilos_T10 = {
-  "text" : "Victorias del jugador",
-  "font" : ('Times New Roman', 12, 'bold'),
-  "foreground" : "black"
-}
-
-estilos_T11 = {
+texto_normal8 = {
   "text" : "Victorias de la computadora",
   "font" : ('Times New Roman', 12, 'bold'),
   "foreground" : "black"
 }
 
-estilos_T12 = {
+texto_normal9 = {
+  "text" : "El ganador del juego es: ",
+  "font" : ('Times New Roman', 12, 'bold'),
+  "foreground" : "black"
+}
+
+firma = {
   "text" : "Hecho por HaCaFe97",
   "font" : ('Times New Roman', 12, 'bold'),
   "foreground" : "gray"
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import random
+from formatos_textos import titulo_nivel1, titulo_nivel2, titulo_nivel3, texto_normal1, texto_normal2, texto_normal3, texto_normal4, texto_normal5, texto_normal6, texto_normal7, texto_normal8, texto_normal9, firma
 
 opciones = ["piedra", "papel", "tijera"]
 victorias_jugador = 0
@@ -69,23 +70,27 @@ img_tijera = tk.PhotoImage(file="Tijera.png").subsample(6)
 frame_titulos = tk.Frame(root)
 frame_titulos.grid(row=0, column=1)
 
-titulo_nv1 = tk.Label(frame_titulos, text="Piedra, Papel o Tijera")
+titulo_nv1 = tk.Label(frame_titulos, titulo_nivel1)
 titulo_nv1.grid(row=0, column=0)
 
-titulo_nv2 = tk.Label(frame_titulos, text="Juega contra la computadora en un juego de piedra, papel o tijera")
+titulo_nv2 = tk.Label(frame_titulos, titulo_nivel2)
 titulo_nv2.grid(row=1, column=0)
 
-titulo_nv3 = tk.Label(frame_titulos, text="Quien gane dos de tres rondas, gana el juego")
+titulo_nv3 = tk.Label(frame_titulos, titulo_nivel3)
 titulo_nv3.grid(row=2, column=0)
 
+# Frame de juego
+frame_juego = tk.Frame(root)
+frame_juego.grid(row=1, column=1)
+
 # Crear el frame para los botones del jugador
-frame_jugador = tk.Frame(root)
-frame_jugador.grid(row=1, column=0, padx=10, pady=10)
+frame_jugador = tk.Frame(frame_juego)
+frame_jugador.grid(row=0, column=0, padx=10, pady=10)
 
 # Crear los botones para piedra, papel y tijera para el jugador
 texto_jugador = tk.Frame(frame_jugador)
 texto_jugador.grid(row=0, column=0, padx=5, pady=5)
-lbl_jugador = tk.Label(texto_jugador, text="Elige una opción")
+lbl_jugador = tk.Label(texto_jugador, texto_normal1)
 lbl_jugador.grid(row=0, column=0, padx=5, pady=5)
 
 botones_jugador = tk.Frame(frame_jugador)
@@ -99,53 +104,53 @@ btn_tijera.grid(row=0, column=2, pady=5)
 
 contadores_jugador = tk.Frame(frame_jugador)
 contadores_jugador.grid(row=2, column=0, padx=5, pady=5)
-lbl_vicsjugador = tk.Label(contadores_jugador, text="Victorias Jugador: ")
+lbl_vicsjugador = tk.Label(contadores_jugador, texto_normal2)
 lbl_vicsjugador.grid(row=0, column=0, padx=5, pady=0)
-lbl_contsvicjugador = tk.Label(contadores_jugador, text=victorias_jugador)
+lbl_contsvicjugador = tk.Label(contadores_jugador, text=victorias_jugador, font=("Arial", 12, "bold"))
 lbl_contsvicjugador.grid(row=0, column=1, padx=5, pady=0)
 
 
 # Crear el frame para los botones de iniciar y reiniciar
-frame_rondas = tk.Frame(root)
-frame_rondas.grid(row=1, column=1, padx=10, pady=10)
+frame_rondas = tk.Frame(frame_juego)
+frame_rondas.grid(row=0, column=1, padx=10, pady=10)
 
 # Crear los botones para iniciar y reiniciar
 btn_iniciar = tk.Button(frame_rondas, text="Iniciar", command=iniciar_juego)
 btn_iniciar.grid(row=0, column=0, pady=5)
-lbl_ronda = tk.Label(frame_rondas, text="Ronda")
+lbl_ronda = tk.Label(frame_rondas, texto_normal3)
 lbl_ronda.grid(row=1, column=0, pady=5)
-lbl_cuentaronda = tk.Label(frame_rondas, text=ronda)
+lbl_cuentaronda = tk.Label(frame_rondas, text=ronda, font=("Arial", 12, "bold"))
 lbl_cuentaronda.grid(row=2, column=0, pady=5)
 
 frame_info = tk.Frame(frame_rondas)
 frame_info.grid(row=3, column=0, pady=5)
 
-lbl_elec_jugador = tk.Label(frame_info, text="El jugador escogió")
+lbl_elec_jugador = tk.Label(frame_info, texto_normal4, borderwidth = 2, relief="groove")
 lbl_elec_jugador.grid(row=0, column=0, padx=5, pady=5)
-lbl_elec_jugador_def = tk.Label(frame_info, text="piedrapapeltijera")
+lbl_elec_jugador_def = tk.Label(frame_info, text=" ", font=("Arial", 12))
 lbl_elec_jugador_def.grid(row=1, column=0, padx=5, pady=5)
 
-lbl_ganaronda = tk.Label(frame_info, text="Ganador de ronda")
+lbl_ganaronda = tk.Label(frame_info, texto_normal5, borderwidth = 2, relief="groove")
 lbl_ganaronda.grid(row=0, column=1, pady=5)
-lbl_ganador = tk.Label(frame_info, text="Ganador")
+lbl_ganador = tk.Label(frame_info, text=" ", font=("Arial", 12))
 lbl_ganador.grid(row=1, column=1, pady=5)
 
-lbl_elec_computadora = tk.Label(frame_info, text="La computadora escogió")
+lbl_elec_computadora = tk.Label(frame_info, texto_normal6, borderwidth = 2, relief="groove")
 lbl_elec_computadora.grid(row=0, column=2, padx=5, pady=5)
-lbl_elec_computadora_def = tk.Label(frame_info, text="piedrapapeltijera")
+lbl_elec_computadora_def = tk.Label(frame_info, text=" ", font=("Arial", 12))
 lbl_elec_computadora_def.grid(row=1, column=2, padx=5, pady=5)
 
 btn_reiniciar = tk.Button(frame_rondas, text="Reiniciar", command=reiniciar_juego)
 btn_reiniciar.grid(row=4, column=0, pady=5)
 
 # Crear el frame para la zona de la computadora
-frame_computadora = tk.Frame(root)
-frame_computadora.grid(row=1, column=2, padx=10, pady=10)
+frame_computadora = tk.Frame(frame_juego)
+frame_computadora.grid(row=0, column=2, padx=10, pady=10)
 
 # Crear los labels para piedra, papel y tijera para la computadora
 texto_computadora = tk.Frame(frame_computadora)
 texto_computadora.grid(row=0, column=0, padx=5, pady=5)
-lbl_computadora = tk.Label(texto_computadora, text="La computadora elegirá")
+lbl_computadora = tk.Label(texto_computadora, texto_normal7)
 lbl_computadora.grid(row=0, column=0, padx=5, pady=5)
 
 labels_computadora = tk.Frame(frame_computadora)
@@ -159,18 +164,26 @@ lbl_tijera.grid(row=0, column=2, pady=5)
 
 contadores_computadora = tk.Frame(frame_computadora)
 contadores_computadora.grid(row=2, column=0, padx=5, pady=5)
-lbl_vicscomputadora = tk.Label(contadores_computadora, text="Victorias Computadora: ")
+lbl_vicscomputadora = tk.Label(contadores_computadora, texto_normal8)
 lbl_vicscomputadora.grid(row=0, column=0, padx=5, pady=0)
-lbl_contsviccomputadora = tk.Label(contadores_computadora, text=victorias_computadora)
+lbl_contsviccomputadora = tk.Label(contadores_computadora, text=victorias_computadora, font=("Arial", 12, "bold"))
 lbl_contsviccomputadora.grid(row=0, column=1, padx=5, pady=0)
 
 #Mensaje de Victoria
 lbl_campeon = tk.Frame(root)
 lbl_campeon.grid(row=2, column=1, padx=0, pady=5)
-lbl_campeontext = tk.Label(lbl_campeon, text="El ganador del juego es: ")
+lbl_campeontext = tk.Label(lbl_campeon, texto_normal9)
 lbl_campeontext.grid(row=0, column=0, padx=0, pady=5)
-lbl_campeondef = tk.Label(lbl_campeon, text="jugadorcomputadora")
+lbl_campeondef = tk.Label(lbl_campeon, text=" " , font=("Arial", 14))
 lbl_campeondef.grid(row=1, column=0, padx=0, pady=5)
+
+# Firma
+frame_firma = tk.Frame(root)
+frame_firma.grid(row=3, column=1, padx=0, pady=5)
+
+lbl_firma = tk.Label(frame_firma, firma)
+lbl_firma.grid(row=0, column=0, padx=0, pady=5)
+
 
 # Ejecutar el bucle principal de la ventana
 root.mainloop()
